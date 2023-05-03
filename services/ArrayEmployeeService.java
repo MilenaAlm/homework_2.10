360080d9e6d4a5330fa2b37da01deb8b6f7e863b
milenaalmazyan <ams007@mail.ru>
2023-05-04 00:58:38 +0300

    домашнее задание 2.10

---
+++ services/ArrayEmployeeService.java
@@ -0,0 +1,62 @@
+package pro.sky.hw_2_10.services;
+import org.springframework.stereotype.Service;
+import pro.sky.hw_2_10.Employee;
+import pro.sky.hw_2_10.Exceptions.EmployeeAlreadyAddedException;
+import pro.sky.hw_2_10.Exceptions.EmployeeNotFoundException;
+import pro.sky.hw_2_10.Exceptions.EmployeeStorageIsFullException;
+
+@Service
+public class ArrayEmployeeService implements EmployeeService{
+    private static final int size  = 10;
+    private final Employee[] employees = new Employee[size];
+
+    private final ValidatorService validatorService;
+
+    public ArrayEmployeeService(ValidatorService validatorService) {
+        this.validatorService = validatorService;
+    }
+
+    @Override
+    public Employee addEmployee(String firstName, String lastName) {
+        int indexForAdd = -1;
+        Employee employee = new Employee(
+                validatorService.validateFirstName(firstName),
+                validatorService.validateLastName(lastName));
+        for (int i = 0; i < employees.length ; i++) {
+            if (employees[i] == null && indexForAdd == -1) {
+                indexForAdd = i;
+            }
+            if(employees[i]!=null && employees.equals(employee)){
+                throw new EmployeeAlreadyAddedException();
+                }
+            }
+            if (indexForAdd ==-1) {
+                throw new EmployeeStorageIsFullException();
+            }
+            employees [indexForAdd] = employee;
+            return employees [indexForAdd];
+        }
+
+    @Override
+    public Employee removeEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        for (int i = 0; i < employees.length; i++) {
+            if (employee.equals(employees[i])) {
+                employees[i] = null;
+                return employee;
+            }
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Employee findEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        for (Employee emp : employees) {
+            if (employees.equals(emp)) {
+                return employee;
+            }
+        }
+        throw new EmployeeNotFoundException();
+    }
+}
